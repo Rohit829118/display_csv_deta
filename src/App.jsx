0ee2616255424201c5adc841f213d9fded36e8ea
Rohit Kumar <rohitkumaryadavptru677@gmail.com>
2023-02-26 22:07:52 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ReactCSV from "./components/ReactCSV";
+
+const App = () => {
+  return (
+    <div className="App">
+      <ReactCSV />
+    </div>
+  );
+};
+
+export default App;
